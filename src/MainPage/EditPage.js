3f2b6dc052369adb60b253224369d7d7c1dc6ccb
wjh <835160049@qq.com>
2022-10-25 17:34:21 +0800

    下班

--- src/MainPage/EditPage.js
+++ src/MainPage/EditPage.js
@@ -48,7 +48,7 @@ export default class EditPage extends React.Component {
     // this.perspectiveCamera(renderer, canvas)
     // this.tankMove(renderer, canvas)
     // this.shadow(renderer, canvas)
-    this.fog(renderer, canvas)
+    // this.fog(renderer, canvas)
     // this.raycaster(renderer, canvas)
     // this.webglRenderTarget(renderer, canvas)
     // this.load_gltf(renderer, canvas)
@@ -60,7 +60,7 @@ export default class EditPage extends React.Component {
     // this.study_raycasting_poingts(renderer, canvas)
     // this.points(renderer, canvas)
 
-    // this.draw_map(renderer, canvas)
+    this.draw_map(renderer, canvas)
     // this.shader_change_color(renderer, canvas)
     // this.shader_particle(renderer, canvas)
     // this.curve_camera(renderer, canvas)
