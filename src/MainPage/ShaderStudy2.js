2fbc6baf824f34fd5c135cb395ac217fe434fe1f
wjh <835160049@qq.com>
2023-01-11 17:21:03 +0800

    地板

--- src/MainPage/ShaderStudy2.js
+++ src/MainPage/ShaderStudy2.js
@@ -2,7 +2,7 @@
  * @Author: Wjh
  * @Date: 2022-09-26 13:03:36
  * @LastEditors: Wjh
- * @LastEditTime: 2023-01-10 17:33:09
+ * @LastEditTime: 2023-01-11 17:20:31
  * @FilePath: \howfar\src\MainPage\ShaderStudy2.js
  * @Description:
  *
@@ -110,7 +110,7 @@ export default class ShaderStudy extends React.Component {
 
     // this.test(renderer)
 
-    this.canvas(renderer) // 地板
+    // this.canvas(renderer) // 地板
 
     // this.label_move(renderer)  // 标签撞墙自动移位
 
@@ -152,10 +152,233 @@ export default class ShaderStudy extends React.Component {
     // camera.position.copy(pos)
     // controls.update();
 
-    let floor = await create_floor();
-    scene.add(floor);
+    let loader = new THREE.TextureLoader();
+
+    // 第一层(网格)
+    let floor1 = await create_floor({
+      img: await loader.loadAsync('/images/1.png'),
+    });
+    scene.add(floor1);
+
+    // 第二层
+    let floor2 = await create_floor({
+      img: await loader.loadAsync('/images/2.png'),
+      imgHighColor: '#00ffff',
+      initOpacity: 0.2,
+      moreLight: 3,
+    });
+    floor2.position.y -= 0.5;
+    scene.add(floor2);
+
+    // 第三层
+    let floor3 = await create_floor({
+      img: await loader.loadAsync('/images/3.png'),
+      imgHighColor: '#ffffff',
+      initOpacity: 0,
+      moreLight: 3,
+    });
+    floor3.position.y -= 1;
+    scene.add(floor3);
 
     async function create_floor(params){
+      let loader = new THREE.TextureLoader();
+
+      let img = await loader.loadAsync('/images/1.png');
+
+      const option = {
+        width: 400,
+        height: 400,
+        img,
+        // 图片重复次数
+        imgRepeatXY: 10,
+        // 图片高亮颜色
+        imgHighColor: '#7a6fc0',
+        // 初始透明度(范围0-1)
+        initOpacity: 0.2, // 
+        // 两个环间隔距离
+        ringDistance: 90,
+
+        // 内环和外环的宽度(范围0-width)
+        blurRadius: 25, 
+        innerRadius: 20,
+        // 内外环里有一个环透明度要低一点
+        lowerOpacity: 0.4,
+        // 最高亮度 * 这个倍数
+        moreLight: 1,
+        // 外围颜色
+        edgeColor: '#1f527b'
+      }
+      Object.assign(option, params);
+      option.img.wrapS = option.img.wrapT = THREE.RepeatWrapping;
+
+      let geometry = new THREE.PlaneGeometry(option.width, option.height, 2, 2);
+      geometry.computeBoundingSphere();
+      let { radius } = geometry.boundingSphere;
+
+      let vTime = { value: 0.0 };
+
+      let mat = new THREE.ShaderMaterial({
+        uniforms: {
+          img: {
+            value: option.img,
+          },
+          imgRepeatXY: {
+            value: option.imgRepeatXY,
+          },
+          radius: {
+            value: radius,
+          },
+          // 内环和外环的宽度
+          vBlurRadius: {
+            value: option.blurRadius,
+          },
+          vInnerRadius: {
+            value: option.innerRadius,
+          },
+          // 内外环里有一个环透明度要低一点
+          vLowerOpacity: {
+            value: option.lowerOpacity
+          },
+          // 初始透明度
+          vInitOpacity: {
+            value: option.initOpacity
+          },
+          // 图片高亮的颜色
+          imgHighColor: {
+            value: new THREE.Color(option.imgHighColor)
+          },
+          // 两个环间隔距离
+          ringDistance: {
+            value: option.ringDistance
+          },
+          moreLight: {
+            value: option.moreLight
+          },
+          edgeColor: {
+            value: new THREE.Color(option.edgeColor)
+          },
+          vTime,
+        },
+        vertexShader: `
+          varying vec2 vUv;
+          varying vec3 vPosition;
+          void main(){
+            vUv = uv;
+            vPosition = position;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+          }
+        `,
+        fragmentShader: `
+          uniform sampler2D img;
+
+          uniform vec3 imgHighColor;
+          uniform vec3 edgeColor;
+
+          uniform float imgRepeatXY;
+
+          uniform float radius;
+          uniform float ringDistance;
+          uniform float vTime;
+
+          uniform float vInnerRadius;
+          uniform float vBlurRadius;
+          uniform float vLowerOpacity;
+          uniform float vInitOpacity;
+          uniform float moreLight;
+
+          varying vec2 vUv;
+          varying vec3 vPosition;
+
+          vec4 addRing(float offsetWidth, vec4 texture1, bool isInner){
+            float moveR = mod(vTime + offsetWidth, radius);
+
+            float edge0 = moveR - vInnerRadius;
+            float edge1 = moveR;
+            float edge2 = moveR + vBlurRadius;
+
+            float curR = length(vPosition.xy);
+
+            float o = vInitOpacity == 0.0 ? 1.0 : texture1.a * (1.0 / vInitOpacity * moreLight);
+
+            // 内环
+            if(curR < edge1 && curR > edge0){
+              float d = smoothstep(edge0, edge1, curR); // 由外到内是 1.0-0.0
+              float d1 = clamp(d * o, 0., 1.); 
+              texture1.a = isInner ? max(d1 * vLowerOpacity, texture1.a) : max(d1, texture1.a);
+              
+              texture1.rgb = mix(texture1.rgb, imgHighColor, d);
+            
+            // 外环
+            }else if(curR < edge2 && curR > edge1){
+              float d = (1.0 - smoothstep(edge1, edge2, curR)); // 由内到外是 1.0-0.0
+              float d1 = clamp(d * o, 0., 1.); 
+              texture1.a = isInner ? max(d1, texture1.a) : max(d1 * vLowerOpacity, texture1.a);
+
+              texture1.rgb = mix(texture1.rgb, imgHighColor, d);
+              
+            }
+            return texture1;
+          }
+          vec4 addRing2(float offsetWidth, vec4 texture1, bool isInner){
+            float moveR = mod(vTime + offsetWidth, radius);
+
+            float edge0 = moveR - vInnerRadius;
+            float edge1 = moveR;
+            float edge2 = moveR + vBlurRadius;
+
+            float curR = length(vPosition.xy);
+
+            // 内环
+            if(curR < edge1 && curR > edge0){
+              float d = smoothstep(edge0, edge1, curR); // 由外到内是 1.0-0.0
+              texture1.a *= isInner ? (d * vLowerOpacity) : d;  
+              
+            // 外环
+            }else if(curR < edge2 && curR > edge1){
+              float d = (1.0 - smoothstep(edge1, edge2, curR)); // 由内到外是 1.0-0.0
+              texture1.a *= isInner ? d : (d * vLowerOpacity);
+
+            }else{
+              texture1.a = 0.0;
+            }
+            return texture1;
+          }
+          void main(){
+            vec4 texture1 = texture2D(img, vec2(vUv.x * imgRepeatXY, vUv.y * imgRepeatXY));
+
+            if(vInitOpacity == 0.0){
+              vec4 ring1 = addRing2(0.0, texture1, true);
+              vec4 ring2 = addRing2(ringDistance, texture1, false);
+              gl_FragColor = ring1 + ring2;
+
+            }else{
+              
+              texture1.a *= vInitOpacity;
+              vec4 ring1 = addRing(0.0, texture1, true);
+              vec4 ring2 = addRing(ringDistance, ring1, false);
+              gl_FragColor = ring2;
+            }
+
+            float o = length(vPosition.xy/radius);
+            gl_FragColor = vec4(mix(gl_FragColor.rgb, edgeColor, o), gl_FragColor.a);
+          }
+        `,
+        transparent: true,
+      });
+
+      let clock = new THREE.Clock();
+      function render() {
+        vTime.value += clock.getDelta() * 20;
+        requestAnimationFrame(render);
+      }
+      render();
+
+      let mesh = new THREE.Mesh(geometry, mat)
+      mesh.rotateX(-Math.PI * 0.5);
+      return mesh;
+    }
+
+    async function create_floor1(params){
 
       let loader = new THREE.TextureLoader();
 
@@ -173,15 +396,19 @@ export default class ShaderStudy extends React.Component {
         img2RepeatXY: 10,
         img3,
         img3RepeatXY: 10,
+
+        // 内环和外环的宽度
+        blurRadius: 20,
+        innerRadius: 20,
       }
       Object.assign(option, params);
 
-      let geometry = new THREE.PlaneGeometry(option.width, option.height)
+      let geometry = new THREE.PlaneGeometry(option.width, option.height, 2, 2)
       geometry.computeBoundingSphere();
       geometry.computeBoundingBox();
       let { center, radius } = geometry.boundingSphere;
       let { min, max } = geometry.boundingBox;
-      console.log(geometry.boundingBox);
+      console.log(geometry);
 
       let vTime = { value: 0.0 };
 
@@ -206,13 +433,25 @@ export default class ShaderStudy extends React.Component {
             value: option.img3RepeatXY,
           },
           radius: {
-            value: radius,
+            value: new THREE.Vector2((max.x - min.x)/2, (max.y - min.y)/2),
           },
           center: {
             value: center,
           },
-          uResolution: {
-            value: new THREE.Vector2(max.x - min.x, max.y - min.y)
+          // 内环和外环的宽度
+          vBlurRadius: {
+            value: 30,
+          },
+          vInnerRadius: {
+            value: 30,
+          },
+          // 内外环里有一个环透明度要低一点
+          vLowerOpacity: {
+            value: 0.4
+          },
+          // 图片1高亮的颜色
+          img1HighColor: {
+            value: new THREE.Color('#7a6fc0')
           },
           vTime,
         },
@@ -230,45 +469,96 @@ export default class ShaderStudy extends React.Component {
           uniform sampler2D img2;
           uniform sampler2D img3;
 
+          uniform vec3 img1HighColor;
+
           uniform float img1RepeatXY;
           uniform float img2RepeatXY;
           uniform float img3RepeatXY;
 
-          uniform float radius;
+          uniform vec2 radius;
           uniform vec3 center;
           uniform float vTime;
 
-          uniform vec2 uResolution;
+          uniform float vInnerRadius;
+          uniform float vBlurRadius;
+          uniform float vLowerOpacity;
 
           varying vec2 vUv;
+          varying vec3 vPosition;
 
-          void addRing(float offsetWidth, bool isInner){
-            float moveR = mod(vTime + offsetWidth, radius);
+          vec4 addRing(float offsetWidth, vec4 texture1, vec4 texture2, vec4 texture3){
+            float moveR = mod(vTime + offsetWidth, radius.x);
 
-            vec2 st = gl_FragCoord.xy / uResolution;
-            st -= 0.5;
-            float r = length(st);
+            float edge0 = moveR - vInnerRadius;
+            float edge1 = moveR;
+            float edge2 = moveR + vBlurRadius;
+
+            float curR = length(vPosition.xy);
+
+            // 内环
+            if(curR < edge1 && curR > edge0){
+              float d = smoothstep(edge0, edge1, curR); // 由外到内是 1.0-0.0
+              float d1 = clamp(d * texture1.a * 5.0, 0., 1.); 
+              texture1.a = max(d1 * vLowerOpacity, texture1.a);
+              
+              texture1.rgb = mix(texture1.rgb, img1HighColor, d);
+
+              texture2.a = max(texture2.a, d * texture2.a * 20.0 * vLowerOpacity );
+
+              texture3.a *= d;
             
+            // 外环
+            }else if(curR < edge2 && curR > edge1){
+              float d = (1.0 - smoothstep(edge1, edge2, curR)); // 由内到外是 1.0-0.0
+              float d1 = clamp(d * texture1.a * 5.0, 0., 1.); 
+              texture1.a = max(d1, texture1.a);
+
+              texture1.rgb = mix(texture1.rgb, img1HighColor, d);
+              
+              texture2.a = max(texture2.a, d * texture2.a * 20.0);
+              
+              texture3.a *= d;
+            }else{
+              texture3.a = 0.0;
+            }
+            return texture1 + texture2 + texture3;
+
           }
           void main(){
             vec4 texture1 = texture2D(img1, vec2(vUv.x * img1RepeatXY, vUv.y * img1RepeatXY));
             vec4 texture2 = texture2D(img2, vec2(vUv.x * img2RepeatXY, vUv.y * img2RepeatXY));
             vec4 texture3 = texture2D(img3, vec2(vUv.x * img3RepeatXY, vUv.y * img3RepeatXY));
 
+            // 初始化三个贴图的参数
             texture2.rgb = vec3(0.0, 1.0, 1.0);
+            texture1.a *= 0.2;
+            texture2.a *= 0.1;
 
-            if(texture1.a < 0.01){
+            if(texture1.a < 0.001){
               texture1.rgb = vec3(0.0, 0.0, 0.0);
             }
-            if(texture2.a < 0.01){
+            if(texture2.a < 0.001){
               texture2.rgb = vec3(0.0, 0.0, 0.0);
             }
+            if(texture3.a < 0.001){
+              texture3.rgb = vec3(0.0, 0.0, 0.0);
+            }
 
-            gl_FragColor = texture1 + texture2 + texture3;
+            vec4 ring2 = addRing(0.0, texture1, texture2, texture3);
+            vec4 ring1 = addRing(radius.x / 2.0, texture1, texture2, texture3);
+
+            gl_FragColor = ring1 + ring2;
           }
         `,
         transparent: true,
       });
+
+      let clock = new THREE.Clock();
+      function render() {
+        vTime.value += clock.getDelta() * 20;
+        requestAnimationFrame(render);
+      }
+      render();
 
       let mesh = new THREE.Mesh(geometry, mat)
       mesh.rotateX(-Math.PI * 0.5);
